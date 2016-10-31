d4c652ab85b0b58c47ce48d524a1525f1c3e1a4d
Sung Kim <sungkim@Sungs-MBP.wv.cc.cmu.edu>
2016-10-30 20:40:05 -0400

    basic visualization functionality finished

--- appointment_report_vis.py
+++ appointment_report_vis.py
@@ -11,27 +11,27 @@ APPTS_COLUMN = 2
 FIRST_DATA_COLUMN = 3;
 LAST_DATA_COLUMN = 6;
 
-departments= []
-visit_types = []
-appts = []
-data = []
+departments= {}
+
+def add_row(row):
+    department= row[DEPARTMENT_COLUMN].strip()
+    if(not departments.has_key(department)):
+        departments[department] = ([],[],[])
+    visit_types, appts, data = departments[department]
+    visit_types.append(row[VISIT_TYPE_COLUMN])
+    appts.append(row[APPTS_COLUMN])
+    #get rid of the percent sign at the end of each number
+    data.append([float(x[0:len(x)-1])
+        for x in row[FIRST_DATA_COLUMN:LAST_DATA_COLUMN + 1]])
+
 
 with open(file_name, "rb") as f:
     csvReader = csv.reader(f)
     headers = csvReader.next()
-    data_col = "# Appts"
-    #TODO: wrap the following line in a try-catch that can handle
-    #the StopIteration exception
-    data_index = next(i for i in xrange(0, len(headers)) 
-            if headers[i] == data_col)
     for row in csvReader:
-        if(row[data_index] != ""):
-            departments.append(row[DEPARTMENT_COLUMN])
-            visit_types.append(row[VISIT_TYPE_COLUMN])
-            appts.append(row[APPTS_COLUMN])
-
-            data.append([float(x[0:len(x)-1]) for x in row[FIRST_DATA_COLUMN:
-                LAST_DATA_COLUMN + 1]])
+        #data is present
+        if(row[APPTS_COLUMN] != ""):
+            add_row(row)
 
 # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
@@ -45,118 +45,28 @@ for i in range(len(tableau20)):
     r, g, b = tableau20[i]    
     tableau20[i] = (r / 255., g / 255., b / 255.)
 
-for datum in data:
-    plt.pie(datum,labels=headers[FIRST_DATA_COLUMN:LAST_DATA_COLUMN+1], colors=tableau20[0:LAST_DATA_COLUMN - FIRST_DATA_COLUMN + 1],autopct='%1.1f%%',shadow=True,startangle=90)
-    plt.axis('equal')
-    plt.show()
-sys.exit()
-# You typically want your plot to be ~1.33x wider than tall. This plot is a rare    
-# exception because of the number of lines being plotted on it.    
-# Common sizes: (10, 7.5) and (12, 9)    
-#plt.figure(figsize=(12, 14))    
-  
-# Remove the plot frame lines. They are unnecessary chartjunk.    
-#ax = plt.subplot(111)    
-#ax.spines["top"].set_visible(False)    
-#ax.spines["bottom"].set_visible(False)    
-#ax.spines["right"].set_visible(False)    
-#ax.spines["left"].set_visible(False)    
-  
-# Ensure that the axis ticks only show up on the bottom and left of the plot.    
-# Ticks on the right and top of the plot are generally unnecessary chartjunk.    
-#ax.get_xaxis().tick_bottom()    
-#ax.get_yaxis().tick_left()    
-  
-# Limit the range of the plot to only where the data is.    
-# Avoid unnecessary whitespace.    
-#plt.ylim(0, 90)    
-#plt.xlim(1968, 2014)    
-  
-# Make sure your axis ticks are large enough to be easily read.    
-# You don't want your viewers squinting to read your plot.    
-#plt.yticks(range(0, 91, 10), [str(x) + "%" for x in range(0, 91, 10)], fontsize=14)    
-#plt.xticks(fontsize=14)    
-  
-# Provide tick lines across the plot to help your viewers trace along    
-# the axis ticks. Make sure that the lines are light and small so they    
-# don't obscure the primary data lines.    
-#for y in range(10, 91, 10):    
-#    plt.plot(range(1968, 2012), [y] * len(range(1968, 2012)), "--", lw=0.5, color="black", alpha=0.3)    
-  
-# Remove the tick marks; they are unnecessary with the tick lines we just plotted.    
-#plt.tick_params(axis="both", which="both", bottom="off", top="off",    
-#                labelbottom="on", left="off", right="off", labelleft="on")    
-  
-# Now that the plot is prepared, it's time to actually plot the data!    
-# Note that I plotted the majors in order of the highest % in the final year.    
-#majors = ['Health Professions', 'Public Administration', 'Education', 'Psychology',    
-#          'Foreign Languages', 'English', 'Communications\nand Journalism',    
-#          'Art and Performance', 'Biology', 'Agriculture',    
-#          'Social Sciences and History', 'Business', 'Math and Statistics',    
-#          'Architecture', 'Physical Sciences', 'Computer Science',    
-#          'Engineering']    
-  
-#for rank, column in enumerate(majors):    
-    # Plot each line separately with its own color, using the Tableau 20    
-    # color set in order.    
-#    plt.plot(gender_degree_data.Year.values,    
-#            gender_degree_data[column.replace("\n", " ")].values,    
-#            lw=2.5, color=tableau20[rank])    
-  
-    # Add a text label to the right end of every line. Most of the code below    
-    # is adding specific offsets y position because some labels overlapped.    
-#    y_pos = gender_degree_data[column.replace("\n", " ")].values[-1] - 0.5    
-#    if column == "Foreign Languages":    
-#        y_pos += 0.5    
-#    elif column == "English":    
-#        y_pos -= 0.5    
-#    elif column == "Communications\nand Journalism":    
-#        y_pos += 0.75    
-#    elif column == "Art and Performance":    
-#        y_pos -= 0.25    
-#    elif column == "Agriculture":    
-#        y_pos += 1.25    
-#    elif column == "Social Sciences and History":    
-#        y_pos += 0.25    
-#    elif column == "Business":    
-#        y_pos -= 0.75    
-#    elif column == "Math and Statistics":    
-#        y_pos += 0.75    
-#    elif column == "Architecture":    
-#        y_pos -= 0.75    
-#    elif column == "Computer Science":    
-#        y_pos += 0.75    
-#    elif column == "Engineering":    
-#        y_pos -= 0.25    
-  
-    # Again, make sure that all labels are large enough to be easily read    
-    # by the viewer.    
-#    plt.text(2011.5, y_pos, column, fontsize=14, color=tableau20[rank])    
-  
-# matplotlib's title() call centers the title on the plot, but not the graph,    
-# so I used the text() call to customize where the title goes.    
-  
-# Make the title big enough so it spans the entire plot, but don't make it    
-# so big that it requires two lines to show.    
-  
-# Note that if the title is descriptive enough, it is unnecessary to include    
-# axis labels; they are self-evident, in this plot's case.    
-#plt.text(1995, 93, "Percentage of Bachelor's degrees conferred to women in the U.S.A."    
-#       ", by major (1970-2012)", fontsize=17, ha="center")    
-  
-# Always include your data source(s) and copyright notice! And for your    
-# data sources, tell your viewers exactly where the data came from,    
-# preferably with a direct link to the data. Just telling your viewers    
-# that you used data from the "U.S. Census Bureau" is completely useless:    
-# the U.S. Census Bureau provides all kinds of data, so how are your    
-# viewers supposed to know which data set you used?    
-#plt.text(1966, -8, "Data source: nces.ed.gov/programs/digest/2013menu_tables.asp"    
-#       "\nAuthor: Randy Olson (randalolson.com / @randal_olson)"    
-#       "\nNote: Some majors are missing because the historical data "    
-#       "is not available for them", fontsize=10)    
-  
-# Finally, save the figure as a PNG.    
-# You can also save it as a PDF, JPEG, etc.    
-# Just change the file extension in this call.    
-# bbox_inches="tight" removes all the extra whitespace on the edges of your plot.    
-plt.savefig("test.png", bbox_inches="tight")
+import numpy as np
+import os
+N = LAST_DATA_COLUMN - FIRST_DATA_COLUMN + 1
+ind = np.arange(N)
+for department, rows in departments.iteritems():
+    fig, ax = plt.subplots(figsize=(20,10))
+    subplots = []
+    width = 0.05
+    i = 0
+    visits, appts, data = rows
+    for datum in data:
+        subplots.append(ax.bar(ind + i * width,
+            datum, width, color = tableau20[i%len(tableau20)]))
+        i += 1
+    ax.set_ylabel('percentages')
+    ax.set_xticks(ind + width)
+    ax.set_xticklabels(headers[FIRST_DATA_COLUMN:LAST_DATA_COLUMN+1])
+    ax.set_title(department)
+    ax.legend([subplot[0] for subplot in subplots],
+        [visit for visit in visits])
+    if(not os.path.isdir(os.getcwd() + "/output")):
+        os.mkdir("output")
+    plt.savefig("output/" + department + '.png', bbox_inches='tight')
+
+

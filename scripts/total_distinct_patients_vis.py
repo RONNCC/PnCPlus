1e898ccc157c04c2fa09291c12d9161c2e641e6a
sj <sung.kim@Sungs-MacBook-Pro.local>
2016-11-28 13:19:39 -0500

    Need to fix file saving ode in total_distinct_patients

--- scripts/total_distinct_patients_vis.py
+++ scripts/total_distinct_patients_vis.py
@@ -21,7 +21,7 @@ ACADEMIC_LEVEL= 2
 
 data = []
 
-with open(path, "rb") as f:
+with open(file_name, "rb") as f:
     reader = csv.reader(f)
     headers = reader.next()
 
@@ -56,6 +56,8 @@ y_pos = np.arange(len(labels)) + 0.5
 ax.barh(y_pos, counts, align='center', color=tableau20[1])
 plt.yticks(y_pos, labels)
 plt.xlabel("number of distinct patients")
+plt.show()
+sys.exit()
 
 plt.savefig(output_directory + "/" + title+ " " +
         date + '.eps', bbox_inches='tight')

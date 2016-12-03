96c459ccef85b51ae1ec68d976cc71ded0eedd52
RONNCC <ronnie.ghose@gmail.com>
2016-12-03 17:06:23 +0000

    change scripts to use function form

--- scripts/ctry_orig.py
+++ scripts/ctry_orig.py
@@ -1,86 +1,80 @@
 import matplotlib.pyplot as plt
 import csv, sys, re, os
 import Tkinter, FileDialog
-
-path = sys.argv[1]
-output_path = sys.argv[2]
-title_start = path.rfind(os.path.sep) + 1
-title = path[title_start :len(path)-4]
-
-#date_regex = r'[0-9\-\.]*\.csv$'
-#date = re.search(date_regex, path).group()
-##remove file ending
-#date = date[:len(date)-4]
-output_directory = output_path + os.path.sep + "ctry_orig"
-if(not os.path.isdir(output_directory)):
-    os.mkdir(output_directory)
-
-ACADEMIC_LVL = 0
-INTERNATIONAL_BOOL = 1
-COUNTRY = 2
-
-NUMBER_OF_CTRY_DISPLAYED = 20
-
-from collections import Counter
-
-all_count = Counter()
-international_count = Counter()
-domestic_count = Counter()
-
-with open(path, "rb") as f:
-    reader = csv.reader(f)
-    headers = reader.next()
-
-    for row in reader:
-        country = row[COUNTRY].upper()
-        if(country != ""):
-            acad_lvl = row[ACADEMIC_LVL]
-            international = True if row[INTERNATIONAL_BOOL] == "True" else False
-    
-            all_count[country] += 1
-
-            if(international):
-                international_count[country] += 1
-            else:
-                domestic_count[country] += 1
-
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
-  
-# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
-for i in xrange(len(tableau20)):    
-    r, g, b = tableau20[i]    
-    tableau20[i] = (r / 255., g / 255., b / 255.)
-
-plt.title(title)
-heap = []
-
 import numpy as np
 import heapq
-for country in international_count:
-    count = international_count[country]
-    heap.append((-1 * count, country))
+from collections import Counter
 
-heapq.heapify(heap)
-y_pos = np.arange(NUMBER_OF_CTRY_DISPLAYED) +0.5
-
-counts = []
-countries = []
-for i in xrange(NUMBER_OF_CTRY_DISPLAYED):
-    (count, country) = heapq.heappop(heap)
-    counts.append(-1 * count)
-    countries.append(country)
-
-countries.reverse()
-counts.reverse()
-
-plt.barh(y_pos, counts, align = 'center', color=tableau20[0])
-plt.yticks(y_pos, countries)
-plt.xlabel("number of students")
-
-plt.savefig(output_directory + "/" + title+ " " +
-        '.eps', bbox_inches='tight')
-
+def runReport(path, output_path):
+    
+    title_start = path.rfind(os.path.sep) + 1
+    title = path[title_start :len(path)-4]
+    
+    output_directory = output_path + os.path.sep + "ctry_orig"
+    if(not os.path.isdir(output_directory)):
+        os.mkdir(output_directory)
+    
+    ACADEMIC_LVL = 0
+    INTERNATIONAL_BOOL = 1
+    COUNTRY = 2
+    NUMBER_OF_CTRY_DISPLAYED = 20
+    
+    all_count = Counter()
+    international_count = Counter()
+    domestic_count = Counter()
+    
+    with open(path, "rb") as f:
+        reader = csv.reader(f)
+        headers = reader.next()
+    
+        for row in reader:
+            country = row[COUNTRY].upper()
+            if(country != ""):
+                acad_lvl = row[ACADEMIC_LVL]
+                international = True if row[INTERNATIONAL_BOOL] == "True" else False
+        
+                all_count[country] += 1
+    
+                if(international):
+                    international_count[country] += 1
+                else:
+                    domestic_count[country] += 1
+    
+    tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+      
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
+    for i in xrange(len(tableau20)):    
+        r, g, b = tableau20[i]    
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+    
+    plt.title(title)
+    heap = []
+    
+    for country in international_count:
+        count = international_count[country]
+        heap.append((-1 * count, country))
+    
+    heapq.heapify(heap)
+    y_pos = np.arange(NUMBER_OF_CTRY_DISPLAYED) +0.5
+    
+    counts = []
+    countries = []
+    for i in xrange(NUMBER_OF_CTRY_DISPLAYED):
+        (count, country) = heapq.heappop(heap)
+        counts.append(-1 * count)
+        countries.append(country)
+    
+    countries.reverse()
+    counts.reverse()
+    
+    plt.barh(y_pos, counts, align = 'center', color=tableau20[0])
+    plt.yticks(y_pos, countries)
+    plt.xlabel("number of students")
+    
+    plt.savefig(output_directory + "/" + title+ " " +
+            '.eps', bbox_inches='tight')
+    
--- scripts/total_distinct_patients_vis.py
+++ scripts/total_distinct_patients_vis.py
@@ -3,59 +3,59 @@ import csv, sys, re, os, numpy as np
 import Tkinter, FileDialog
 
 # example file_name:  "Total Distinct Patients 8.19.13-8.18.14.csv"
-file_name = sys.argv[1]
-output_path = sys.argv[2]
-title_start = file_name.rfind(os.path.sep) + 1
-title = file_name[title_start :len(file_name)-4]
 
-date_regex = r'[0-9\-\.]*\.csv$'
-date = re.search(date_regex, file_name).group()
-#remove file ending
-date = date[:len(date)-4]
-output_directory = output_path + os.path.sep + "total_distinct_patients" + date 
-if(not os.path.isdir(output_directory)):
-    os.mkdir(output_directory)
-
-PATIENT_COUNT= 1
-ACADEMIC_LEVEL= 2
-
-data = []
-
-with open(file_name, "rb") as f:
-    reader = csv.reader(f)
-    headers = reader.next()
-
-    for row in reader:
-        data.append(row)
+def runReport(file_name, output_path):
     
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
-  
-# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
-for i in xrange(len(tableau20)):    
-    r, g, b = tableau20[i]    
-    tableau20[i] = (r / 255., g / 255., b / 255.)
-
-fig = plt.figure()
-ax = fig.gca()
-plt.title(title)
-labels=[]
-counts = []
-
-for j in xrange(len(data)):
-    row = data[j]
-    labels.append(row[ACADEMIC_LEVEL] + "(" + row[PATIENT_COUNT] + ")")
-    counts.append(row[PATIENT_COUNT])
-
-y_pos = np.arange(len(labels)) + 0.5
-
-ax.barh(y_pos, counts, align='center', color=tableau20[1])
-plt.yticks(y_pos, labels)
-plt.xlabel("number of distinct patients")
-
-plt.savefig(output_directory + "/" + title+ " " +
-        date + '.eps', bbox_inches='tight')
-
+    title_start = file_name.rfind(os.path.sep) + 1
+    title = file_name[title_start :len(file_name)-4]
+    date_regex = r'[0-9\-\.]*\.csv$'
+    date = re.search(date_regex, file_name).group()
+    #remove file ending
+    date = date[:len(date)-4]
+    output_directory = output_path + os.path.sep + "total_distinct_patients" + date 
+    if(not os.path.isdir(output_directory)):
+        os.mkdir(output_directory)
+    
+    PATIENT_COUNT= 1
+    ACADEMIC_LEVEL= 2
+    
+    data = []
+    
+    with open(file_name, "rb") as f:
+        reader = csv.reader(f)
+        headers = reader.next()
+    
+        for row in reader:
+            data.append(row)
+        
+    tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+      
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
+    for i in xrange(len(tableau20)):    
+        r, g, b = tableau20[i]    
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+    
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.title(title)
+    labels=[]
+    counts = []
+    
+    for j in xrange(len(data)):
+        row = data[j]
+        labels.append(row[ACADEMIC_LEVEL] + "(" + row[PATIENT_COUNT] + ")")
+        counts.append(row[PATIENT_COUNT])
+    
+    y_pos = np.arange(len(labels)) + 0.5
+    
+    ax.barh(y_pos, counts, align='center', color=tableau20[1])
+    plt.yticks(y_pos, labels)
+    plt.xlabel("number of distinct patients")
+    
+    plt.savefig(output_directory + "/" + title+ " " +
+            date + '.eps', bbox_inches='tight')
+    
